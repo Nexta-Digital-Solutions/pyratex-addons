5b328d99c0e4a4fa75d61b6e6047b73d203270a5
VicenteNextads <90673350+VicenteNextads@users.noreply.github.com>
2023-12-29 09:48:53 +0100

    Update __manifest__.py

--- product_product_info/__manifest__.py
+++ product_product_info/__manifest__.py
@@ -9,21 +9,14 @@
 
     'author': "NextaDS",
     'website': "https://nextads.es/",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Product Product',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
+    'version': '16.0.0.1',
+    'license': 'LGPL-3',
     'depends': [
         'base',
         'stock',
         'product'
     ],
-
-    # always loaded
     'data': [
         'security/ir.model.access.csv',
         'views/certifications_view.xml',
