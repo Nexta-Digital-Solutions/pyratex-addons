2dce225473b1265a9073e6d6c2bcb36dcafce1c6
Gfg92 <guillef.dez11@gmail.com>
2024-01-09 15:33:37 +0100

    [DEL] product_product_info

---
+++ ecommerce_filter/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Ecommerce Filter",
+
+    'summary': """
+        """,
+
+    'description': """
+    """,
+
+    'author': "NextaDS",
+    'website': "https://nextads.es/",
+
+    'category': 'Ecommerce',
+    'version': '16.0.0.1',
+    'license': 'LGPL-3',
+
+    'depends': ['base', 'website_sale'],
+
+    'data': [
+        'views/filmstrip_categories_category_views.xml',
+        'views/portrait_categories_views.xml',
+        'views/products_attributes_add_categories_views.xml'
+
+    ]
+}
--- product_product_info/models/product_product.py
+++
@@ -1,9 +0,0 @@
-from odoo import api, fields, models, _
-
-class ProductProduct(models.Model):
-    _inherit = 'product.template'
-
-    fiberfamily_id = fields.Many2one('product.product.fiberfamily', string='Fiber Family')
-    property_id = fields.Many2one('product.product.properties', string='Property')
-    usage_id = fields.Many2many(comodel_name='product.product.usage', string='Usage')
-    certification_id = fields.Many2many(comodel_name='product.product.certifications', string='Certifications')
