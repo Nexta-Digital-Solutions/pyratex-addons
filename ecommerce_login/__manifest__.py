dc16fe1dcbf29cadb95ce6ea02a2a1eca5ae2bc5
Gfg92 <guillef.dez11@gmail.com>
2024-03-27 16:04:50 +0100

    [ADD] login

--- ecommerce_login/__manifest__.py
+++ ecommerce_login/__manifest__.py
@@ -21,19 +21,19 @@
 
     'data': [
         'views/complete_your_profile.xml',
+        'views/billing_address.xml',
+        'views/signup_custom.xml',
     ],
     'demo': [
     ],
     'assets': {
         'web.assets_backend': [
             # 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap'
-            # 'ecommerce_login/static/src/js/navigateToCompleteProfile.js',
         ],
         'web.assets_frontend': [
-            # 'ecommerce_login/static/src/scss/foundation.min.css',
-            # 'ecommerce_login/static/src/js/foundation.min.js',
+            'ecommerce_login/static/src/scss/foundation.min.css',
+            'ecommerce_login/static/src/js/foundation.min.js',
             # 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap',
-            # 'ecommerce_login/static/src/js/navigateToCompleteProfile.js',
         ]
 
     }
--- ecommerce_login/static/src/js/navigateToCompleteProfile.js
+++
@@ -1,5 +0,0 @@
-odoo.define('ecommerce_login.navigateToCompleteProfile', function (require) {
-    "use strict";
-    // window.location.href = '/ecommerce_login/views/complete_your_profile';
-
-});
