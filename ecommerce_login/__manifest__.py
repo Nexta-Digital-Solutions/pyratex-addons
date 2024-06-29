e4e267fcecaf3cfd95e9f1ac2ce34f7fc9b886cc
Vicente Falco <vfalco@nextads.es>
2024-06-29 10:54:56 +0200

    mlnda: añadir jquery.serializejson

--- ecommerce_login/__manifest__.py
+++ ecommerce_login/__manifest__.py
@@ -14,7 +14,7 @@
     'website': "https://nextads.es/",
 
     'category': 'Ecommerce',
-    'version': '16.0.0.4',
+    'version': '16.0.0.5',
     'license': 'LGPL-3',
 
     'depends': [
@@ -39,7 +39,8 @@
         'web.assets_frontend': [
             'ecommerce_login/static/src/js/vue@2.7.14.js',
             'ecommerce_login/static/src/js/vue_instance.js',
-            'ecommerce_login/static/src/scss/signature.scss'
+            'ecommerce_login/static/src/scss/signature.scss',
+            'ecommerce_login/static/src/js/jquery.serializejson.min.js'
             #'ecommerce_login/static/src/scss/foundation.min.css',
             #'ecommerce_login/static/src/js/foundation.min.js',
             # 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap',
