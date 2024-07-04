ca4046fc8598125f52a31f2200527febc8ecd4f7
jbasiero <jbasiero@gmail.com>
2024-07-04 12:37:25 +0200

    [ADD] remove from cart

--- ecommerce_product/static/src/js/remove_from_cart.js
+++ ecommerce_product/static/src/js/remove_from_cart.js
@@ -6,13 +6,13 @@ odoo.define('ecommerce_product.remove_from_cart', function (require) {
 
     publicWidget.registry.WebsiteSale.include({
         _onClickDeleteProduct: function (ev) {
-            ev.preventDefault();
-            var $row = $(ev.currentTarget).closest('tr');
-            var productName = $row.find('.td-product_name').text().trim();
-
-            if (productName === 'Customized Swatchpack') {
-                $row.find('.js_quantity').val(0).trigger('change');
-            }
+//            ev.preventDefault();
+//            var $row = $(ev.currentTarget).closest('tr');
+//            var productName = $row.find('.td-product_name').text().trim();
+//
+//            if (productName === 'Customized Swatchpack') {
+//                $row.find('.js_quantity').val(0).trigger('change');
+//            }
         },
     });
 });
