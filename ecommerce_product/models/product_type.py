43453808f4695ab260ad838de325c5548d182e23
Vicente Falco <vfalco@nextads.es>
2024-07-11 22:02:35 +0200

    ecommerce_product: product_type con type-of_order field

--- ecommerce_product/models/product_type.py
+++ ecommerce_product/models/product_type.py
@@ -6,4 +6,21 @@ class ProductType(models.Model):
     _name = 'product.type'
     _description = 'Modelo para añadir tres tipos de productos'
 
+    TYPES = [
+        ('Swatches', 'Swatches'),
+        ('Stock', 'Stock'),
+        ('Stock Reservation', 'Stock Reservation'),
+        ('SMS', 'SMS'),
+        ('Production', 'Production'),
+        ('Production Studio Fabric', 'Production Studio Fabric'),
+        ('Developments', 'Developments'),
+        ('Other revenue', 'Other Revenue'),
+        ('Lab Dips', 'Lab Dips'),
+        ('Consultancy & Commissions', 'Consultancy & Commissions'),
+        ('Recovo', 'Recovo'),
+        ('e-shop Swatches', 'e-shop Swatches'),
+        ('e-shop Stock', 'e-shop Stock')
+    ]
+
     name = fields.Char(string='Nombre')
+    type_of_order = fields.Selection (TYPES, string = "Type_of_order", default=TYPES[0][0])
