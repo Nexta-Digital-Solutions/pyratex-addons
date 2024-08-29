6445bb9e68dcca3779d25a4d5f9752fba5d01438
Vicente Falco <vfalco@nextads.es>
2024-08-29 08:04:59 +0200

    e_commerce_product: quitar field weigth por x_studio_weigth

--- ecommerce_product/models/product_template.py
+++ ecommerce_product/models/product_template.py
@@ -5,7 +5,6 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     country_id = fields.Many2one('res.country', string='Made in')
-    weight = fields.Float(string='Weight', digits = (4,2))
     width = fields.Float(string='Width', digits = (4,2))
     description_ecommerce = fields.Text(string='Ecommerce Sales Description')
     fiberfamily_id = fields.Many2one('fiber.family', string='Fiber Family')
