c83cca1ba79a21efac916fa4a42f81cf6954e712
Vicente Falco <vfalco@nextads.es>
2024-09-29 16:05:19 +0200

    ecommerce_filter: add search button eshop page

--- ecommerce_filter/__manifest__.py
+++ ecommerce_filter/__manifest__.py
@@ -12,16 +12,16 @@
     'website': "https://nextads.es/",
 
     'category': 'Ecommerce',
-    'version': '16.0.0.7',
+    'version': '16.0.0.8',
     'license': 'LGPL-3',
 
     'depends': ['base', 'website_sale', 'website_sale_delivery', 'ecommerce_product'],
 
     'data': [
         'views/filmstrip_categories_category_views.xml',
-        'views/portrait_categories_views.xml',
-        'views/products_attributes_add_categories_views.xml',
+        #'views/portrait_categories_views.xml',
         'views/attributes_views.xml',
+        #'views/products_attributes_add_categories_views.xml',
         'views/filter_products_price.xml'
     ],
     'assets':{
